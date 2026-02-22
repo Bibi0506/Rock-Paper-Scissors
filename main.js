f0999d8e4f801ae28fd5f09622cf7f768e367c48
Roberta Capuano <robertacapuano18@gmail.com>
2026-02-22 16:46:33 +0000

    added the playRound function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //write a function that randomly returns rock, paper or scissors 
 
 function getComputerChoice(){
-    choice = Math.random()
+    let choice = Math.random()
     if ( choice <= 0.33){
         return "rock"
     }else if(choice <=0.66){
@@ -15,3 +15,33 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice())
+
+//take user choice and returns it
+
+function getHumanChoice(){
+    return window.prompt()
+}
+
+console.log(getHumanChoice())
+
+//keep the score
+
+let humanScore = 0
+let computerScore = 0
+
+//play a round
+/* human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
+*/
+function playRound(getComputerChoice, getHumanChoice){
+    //play single round
+    if(getComputerChoice == "rock" && getHumanChoice == "paper" || getComputerChoice == "scissors" && getHumanChoice == "rock"){
+        return humanScore += 1
+    }else if (getComputerChoice == "paper" && getHumanChoice == "rock" || getComputerChoice == "rock" && getHumanChoice == "scissors"){
+        return computerScore += 1
+    }else if(getComputerChoice == getHumanChoice){
+        return "Tie!!"
+    }
+
+}
+playRound()
+console.log(humanScore)
